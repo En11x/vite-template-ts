0fc138e9dfcffb3bfba93cb52714cf493719ffa9
pp <pijiaxin@wsdashi.com>
2023-05-30 15:27:17 +0800

    feat: add .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import path from 'path'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), AutoImport()],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'),
+export default ({ mode }) => {
+  console.log('mode', loadEnv(mode, process.cwd()).VITE_BASE_URL);
+
+  return defineConfig({
+    plugins: [vue(), AutoImport()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
     },
-  },
-})
+  });
+};
