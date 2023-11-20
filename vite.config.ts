b7db3dfc6d1e040906922228250dbe4a178aff8c
pp <pijiaxin@wsdashi.com>
2023-11-20 17:30:19 +0800

    chore: up deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
-import path from 'path'
 
 export default ({ mode }) => {
   console.log('mode', loadEnv(mode, process.cwd()).VITE_BASE_URL)
